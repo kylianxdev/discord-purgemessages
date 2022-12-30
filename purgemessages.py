ba5a81be558271912c844f8b198730aaa08396d6
Kylian <119763390+kylianxdev@users.noreply.github.com>
2022-12-30 19:35:20 +0100

    Permissions added

--- purgemessages.py
+++ purgemessages.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
+@commands.has_permissions(manage_messages=True)
 @client.command(name='purge', brief='Deletes a specified number of messages from the current channel')
 async def purge(ctx, amount: int):
   # Delete the specified number of messages
